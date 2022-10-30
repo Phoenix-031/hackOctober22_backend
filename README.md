3cd5b5d5f607ccb558f021a99b0501796e5f2388
Debayan <pradhandebayan@gmail.com>
2022-10-30 10:48:06 +0530

    innitial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backend server for hackathon
+**flow of website**
+https://whimsical.com/flow-qsbank-web-MYMkxpMDgaEC6yrpsZCrCx
---
+++ src/index.js
@@ -0,0 +1,56 @@
+//server imports
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const connectDb = require("../config/db");
+
+const authRoutes = require("../routes/auth.routes");
+const fileRoutes = require("../routes/file.routes");
+const userRoutes = require("../routes/user.routes");
+const listRoutes = require("../routes/general.routes");
+
+const errorHandler = require("../middlewares/error.middleware");
+
+//setting up config config file paths
+dotenv.config({ path: "./config/config.env" });
+
+connectDb();
+
+const app = express();
+// console.log(process.env);
+
+//middlewares
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+	res.status(200).json("server running");
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/files", fileRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/list", listRoutes);
+// app.get("*", checkUser); // using checkUser middleware for checking current user
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 7834;
+
+const server = app.listen(PORT, () => {
+	console.log(`server listening on: http://localhost:${PORT}`);
+});
+
+// unhandled promise rejection
+
+process.on("unhandledRejection", (err) => {
+	console.log(`Error:${err.message}`);
+	console.log("shutting down due to unhandled promise rejection");
+
+	server.close(() => {
+		process.exit(1);
+	});
+});
