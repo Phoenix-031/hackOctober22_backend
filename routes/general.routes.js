b4842f40f2943cdd4c8b1f200ca6ff8ddc4008e7
Debayan <pradhandebayan@gmail.com>
2022-10-30 11:08:37 +0530

    removed files

--- routes/general.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getList, addList } = require("../controllers/list.Controller");
-
-//general routes for frontend features
-router.get("/", getList);
-router.post("/", addList);
-
-module.exports = router;
