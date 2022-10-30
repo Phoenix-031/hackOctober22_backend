0e59e609457f77884c5bc6d94d9830b7a225c1f0
Debayan <pradhandebayan@gmail.com>
2022-10-30 11:30:46 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 //middlewares
 app.use(cors());
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "100mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 
