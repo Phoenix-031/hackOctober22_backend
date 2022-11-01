d5eb6bac7d45c185e14918049b9d631e6d5df16b
Debayan <pradhandebayan@gmail.com>
2022-11-01 15:07:03 +0530

    updated

--- controllers/filter.Controller.js
+++ controllers/filter.Controller.js
@@ -2,31 +2,27 @@ const File = require("../models/file.Schema");
 
 const filterResults = async (req, res, next) => {
 	const { Institute, Year, Semester, Department } = req.body;
-	console.log(req.body);
+
 	try {
 		// File
 		const x = {};
 		if (Institute) {
-			x[Institute] = Institute;
+			x["Institute"] = Institute;
 		}
 		if (Year) {
-			x[Year] = Year;
+			x["Year"] = Year;
 		}
 		if (Semester) {
-			x[Semester] = Semester;
+			x["Semester"] = Semester;
 		}
 		if (Department) {
-			x[Department] = Department;
+			x["Department"] = Department;
 		}
 		const file = await File.find(x);
-		// if (file.Institute || file.Year || file.Semester || file.Department) {
-		// 	res.status(200).json({
-		// 		success: true,
-		// 		file,
-		// 	});
-		// }
-		res.json({ file });
-		console.log({ file });
+		res.json({
+			success: true,
+			files: file,
+		});
 	} catch (err) {
 		next(err);
 	}
